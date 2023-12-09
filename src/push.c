52350728e63ab9db940e626358e32a611f76842a
mxvements <luciama2@student.42madrid.com>
2023-12-09 21:46:30 +0100

    comments on push

---
+++ src/push.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luciama2 <luciama2@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/09 21:30:51 by luciama2          #+#    #+#             */
+/*   Updated: 2023/12/09 21:30:52 by luciama2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/push_swap.h"
+
+void	push_a(t_dll **stack_a, t_dll **stack_b)
+{
+	//protections
+
+	//remove first node from b
+	//addfront to a
+	
+	//print
+	ft_putstr_fd("pa\n", 1);
+	//check final state?
+}
