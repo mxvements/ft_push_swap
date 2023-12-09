7fa0bab210ce1637bac91bddabc207a51edd82ff
mxvements <luciama2@student.42madrid.com>
2023-12-09 21:53:52 +0100

    push_a

--- src/push.c
+++ src/push.c
@@ -14,11 +14,15 @@
 
 void	push_a(t_dll **stack_a, t_dll **stack_b)
 {
+	t_content	*tmp;
+	t_dll		*node;
+
 	//protections
 
-	//remove first node from b
-	//addfront to a
-	
+	//
+	tmp = (t_content *)s_pop(stack_b); //to check
+	node = ft_dllnew((void *)(tmp));
+	s_push(stack_a, node); //to check
 	//print
 	ft_putstr_fd("pa\n", 1);
 	//check final state?
